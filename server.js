03c992f96bb84956d283957eb090979f35830204
Nathan Monte <nathan.monte.kh@gmail.com>
2017-02-26 12:37:06 +0000

    Connected to the Bing API to get images.

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 "use strict";
-var app = require('express').express();
+
+const express = require('express');
+var app = express();
 const mongo = require('mongod').MongoClient;
 const bodyParser = require('body-parser');
 const path = require('path');
+const requestify = require('requestify');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -11,3 +14,29 @@ app.set('view engine', 'html');
 
 //Setup for public folder
 app.use(express.static(path.join(__dirname, 'public')));
+const port = process.env.PORT || "8080";
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+})
+
+app.get("/", (req, res, next) => {
+  requestify.request('https://api.cognitive.microsoft.com/bing/v5.0/images/search', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'multipart/form-data',
+          'Ocp-Apim-Subscription-Key':"f7a7970e35124d4d81a3fed709b578c8 "
+      },
+      dataType: 'form-url-encoded',
+      params: {
+        q: "lolcat"
+      }
+  }).then((response) => {
+      // Get the response body (JSON parsed or jQuery object for XMLs)
+      console.log(response.getBody().queryExpansions);
+      res.send(response.getBody().queryExpansions);
+  }).catch((err) => {
+    console.log(err);
+    res.send({});
+  })
+
+})
